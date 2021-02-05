d57ef792ed6467d724f61dcd94af81052002daba
leroy <leroy20317@qq.com>
2021-02-05 18:15:33 +0800

    fix: mongoose

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,5 +6,5 @@ export default {
   port: 5001,
   host: isPro ? '0.0.0.0' : 'localhost',
   // mongodb: 'mongodb://localhost:27017/blogs',
-  mongodb: isPro ? 'mongodb://0.0.0.0:27017/blogs' : 'mongodb://152.136.170.96:27017/blogs',
+  mongodb: isPro ? 'mongodb://0.0.0.0:27017/blogs' : 'mongodb://152.136.170.96:27017/blogs-test',
 }
